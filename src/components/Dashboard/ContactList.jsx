5920683d4bda2f25838fe3e16a451cdefaf94d69
Axel Ahlander <axel.ahlander@gmail.com>
2025-02-12 12:51:39 +0100

    allowed filtered search by name

--- src/components/Dashboard/ContactList.jsx
+++ src/components/Dashboard/ContactList.jsx
@@ -8,6 +8,16 @@ import { useNavigate } from 'react-router-dom';
 function ContactList() {
     const { data, setData, url } = useContext(ContactsContext);
     const [id, setId] = useState("");
+    const [searchBar, setSearchBar] = useState("");
+
+
+    let searchWords = searchBar.trim().toLowerCase().split(" ");
+
+    let currData = data.filter((contact) => {
+        let fullName = `${contact.firstName} ${contact.lastName}`.toLowerCase().trim();
+        return searchWords.every(word => fullName.includes(word));
+    });
+    
 
     const navigate = useNavigate();
 
@@ -20,38 +30,92 @@ function ContactList() {
         e.preventDefault();
         navigate(`/contact/${id}`)
     }
+
+    const handleFilter = (event) => {
+        setSearchBar(event.target.value.toLowerCase());
+    }
+
+    if (searchBar === ""){
+        return (
+            <>
+            <div className='contact-list'>
+                <h1>Contact List</h1>
+                <Link to="/">
+                    <p id="return">Return to menu</p>
+                </Link>
+                <form onSubmit={submitHandler} className="search-form">
+                <div className="input-button-wrapper">
+                    <input 
+                    type="text" 
+                    placeholder="Enter ID" 
+                    onChange={handleInputChange} 
+                    name="id-identifier" 
+                    value={id}
+                    />
+                    <button type="submit">Search</button>
+                    
+                </div>
+                
+                </form>
+                <form className = "search-form" action="">
+                    <div className='input-button-wrapper'>
+                        <input type="text" placeholder='Search by name' onChange={handleFilter}/>
+                    </div>
+                </form>
     
-    return (
-        <>
-        <div className='contact-list'>
-            <h1>Contact List</h1>
-            <Link to="/">
-                <p id="return">Return to menu</p>
-            </Link>
-            <form onSubmit={submitHandler} className="search-form">
-            <div className="input-button-wrapper">
-                <input 
-                type="text" 
-                placeholder="Enter ID" 
-                onChange={handleInputChange} 
-                name="id-identifier" 
-                value={id}
-                />
-                <button type="submit">Search</button>
+    
+    
+                <ul>
+                    {data.map((content, index) => (
+                        <ContactListItem key={index} contact={content} url = {url} setData={setData}  />
+                    ))}
+                </ul>
             </div>
-            </form>
-
-
-
-            <ul>
-                {data.map((content, index) => (
-                    <ContactListItem key={index} contact={content} url = {url} setData={setData}  />
-                ))}
-            </ul>
-        </div>
-        </>
-        
-    );
+            </>
+            
+        );
+    }
+    
+    else {
+        return (
+            <>
+            <div className='contact-list'>
+                <h1>Contact List</h1>
+                <Link to="/">
+                    <p id="return">Return to menu</p>
+                </Link>
+                <form onSubmit={submitHandler} className="search-form">
+                <div className="input-button-wrapper">
+                    <input 
+                    type="text" 
+                    placeholder="Enter ID" 
+                    onChange={handleInputChange} 
+                    name="id-identifier" 
+                    value={id}
+                    />
+                    <button type="submit">Search</button>
+                    
+                </div>
+                
+                </form>
+                <form className = "search-form" action="">
+                    <div className='input-button-wrapper'>
+                        <input type="text" placeholder='Search by name' onChange={handleFilter}/>
+                    </div>
+                </form>
+    
+    
+    
+                <ul>
+                    {currData.map((content, index) => (
+                        <ContactListItem key={index} contact={content} url = {url} setData={setData}  />
+                    ))}
+                </ul>
+            </div>
+            </>
+            
+        );
+    }
 }
 
 export default ContactList;
